06431672f58dbb715657d6e539903e1a0fc4e473
Mark Kramer <mkramer@expedia.com>
2017-05-01 11:13:42 -0700

    Fix output to UI

--- src/app/react-components/HelloReactWorld.tsx
+++ src/app/react-components/HelloReactWorld.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const HelloReact = ({name}) => <p>Hello from react {name}!</p>;
+const HelloReact = ({name}) => <p>Hello {name}! Greetings from your react component!</p>;
 
 export default function RenderHelloReactWorld(selector, props){
   ReactDOM.render(React.createElement(HelloReact, props),
