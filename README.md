ee4c031d54872aceda917cae2eeccb28c434c754
Mark Kramer <mkramer@expedia.com>
2017-05-01 16:27:02 -0700

    Update tests to know where to look for files

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-Run `ng run build` for or `ng run build:prod` to generate a production build
 
 ## Code scaffolding
 
@@ -14,7 +13,8 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 ## Build
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
+Run `ng run build`. The build artifacts will be stored in the `dist/` directory.
+Run `ng run build:prod` to generate a production build 
 
 ## Running unit tests
 
--- karma.conf.js
+++ karma.conf.js
@@ -4,9 +4,10 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular/cli', 'karma-typescript'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-typescript'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
@@ -16,7 +17,8 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     files: [
-      { pattern: './src/test.ts', watched: false }
+      { pattern: './src/app/*.ts', watched: false },
+      { pattern: './src/app/react-components/*.tsx', watched: false }
     ],
     preprocessors: {
       './src/test.ts': ['@angular/cli']
