2c66cb7eef236dd913872449026f9be174c12df5
Mark Kramer <mkramer@expedia.com>
2017-05-01 10:18:22 -0700

    Import the react component and use in an Angular component. Install webpack-dev-server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import RenderHelloReactWorld from './react-components/HelloReactWorld';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'app works!';
+
+  ngOnInit(): void {
+    RenderHelloReactWorld('#reactHost', { name: 'Fred Flintstone' });
+  }
 }
